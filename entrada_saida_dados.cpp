2616a8dc203e89384a05b8a28c7cd957c32a6d5a
ricardojjs <ricardojjs135>
2024-10-16 09:37:19 -0300

    fix: correcao em nome do arquivo

---
+++ entrada_saida_dados.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() { 
+  string nome;
+  int idade;
+  double altura;
+
+  cout << "Digite seu nome: ";
+  cin >> nome;
+  
+  cout << "Digite sua idade: ";
+  cin >> idade;
+  
+  cout << "Digite sua altura (X.XX): ";
+  cin >> altura;
+
+  stringstream ss;
+  ss << fixed << setprecision(2) << altura;
+  string alturaFormatada = ss.str();
+
+  string mensagem = "Olá, " + nome + "! Você tem " + to_string(idade) + " anos e sua altura é " + alturaFormatada + " metros.";
+
+  cout << mensagem << std::endl;
+
+}
